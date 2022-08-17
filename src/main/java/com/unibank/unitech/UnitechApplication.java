1c4905bfa6a325f886c57891fa4933f67cfcbe27
orxanabdullazade <orxanzadeh@gmail.com>
2022-08-17 12:09:24 +0400

    Initial commit

---
+++ src/main/java/com/unibank/unitech/UnitechApplication.java
@@ -0,0 +1,13 @@
+package com.unibank.unitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitechApplication.class, args);
+	}
+ ///test
+}
